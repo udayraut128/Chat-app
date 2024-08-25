1d899876e3dc4ca45a710594aa0cd790ce062850
coderroot2 <udayraut128@gmail.com>
2024-08-25 21:55:58 +0530

    add wechat

--- 03_Frontend_and_Backend_Both/Chat-App/public/src/utils/APIRoutes.js
+++
@@ -1,2 +0,0 @@
-export const host = "http://localhost:5000";
-export const registerRoute = `${host}/api/auth/register`;
--- 03_Frontend_and_Backend_Both/Chat-App/server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const {register} =require("../controllers/userControllers");
-
-const router = require("express").Router();
-
-router.post("/register",register);
-
-module.exports = router;
